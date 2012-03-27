dcc8695b7be9efb51815b164c5894469d0ad9713
Bill Rosgen <rosgen@gmail.com>
2012-03-27 19:24:12 +0800

    Add AJAX to update Scite button, counter, etc

--- app/controllers/scites_controller.rb
+++ app/controllers/scites_controller.rb
@@ -4,13 +4,20 @@ class ScitesController < ApplicationController
   def create
     @paper = Paper.find(params[:scite][:paper_id])
     current_user.scite!(@paper)
-    redirect_to(:back)    
+
+    respond_to do |format|
+      format.html { redirect_to(:back) }
+      format.js
+    end
   end
 
   def destroy    
     @paper = Paper.find(params[:paper_id])
-
     current_user.unscite!(@paper)
-    redirect_to(:back)    
+
+    respond_to do |format|
+      format.html { redirect_to(:back) }
+      format.js
+    end
   end
 end
---
+++ spec/controllers/scites_controller_spec.rb
@@ -0,0 +1,42 @@
+describe ScitesController do
+
+  let(:user)  { FactoryGirl.create(:user) }
+  let(:paper) { FactoryGirl.create(:paper) }
+
+  before { sign_in user }
+
+  describe "creating a scite with Ajax" do
+
+    before { request.env['HTTP_REFERER'] = 'scites/new' }
+
+    it "should increment the Scite count" do
+      expect do
+        xhr :post, :create, scite: { paper_id: paper.id }
+      end.should change(Scite, :count).by(1)
+    end
+
+    it "should respond with success" do
+      xhr :post, :create, scite: { paper_id: paper.id }
+      response.should be_success
+    end
+  end
+
+  describe "destroying a relationship with Ajax" do
+
+    before do
+      user.scite!(paper)
+      request.env['HTTP_REFERER'] = 'scites/new'
+    end
+
+    it "should decrement the Scite count" do
+      expect do
+        xhr :delete, :destroy, paper_id: paper.id
+      end.should change(Scite, :count).by(-1)
+    end
+
+    it "should respond with success" do
+      xhr :delete, :destroy, paper_id: paper.id
+      response.should be_success
+    end
+  end
+end
