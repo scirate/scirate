18643a86b0a7ff28f19ffb81da74545697b70c33
Mispy <scirate@mispy.me>
2013-05-18 20:26:31 +1000

    user_edit_path -> settings_path in test

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -19,7 +19,7 @@ describe "Authentication" do
 
       it { should have_title user.name }
       it { should have_link('Profile', href: user_path(user)) }
-      it { should have_link('Settings', href: edit_user_path(user)) }
+      it { should have_link('Settings', href: settings_path) }
       it { should have_link('Subscriptions', href: subscriptions_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
