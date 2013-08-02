b85d44ccc02d22714a4f21b2d98ef800a25488d2
Mispy <scirate@mispy.me>
2013-08-02 17:29:36 +1000

    Silence deprecation warnings from external gems

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,9 @@ require 'exception_notifier'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  ActiveSupport::Deprecation.silence do
+    Bundler.require(*Rails.groups(:assets => %w(development test)))
+  end
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
