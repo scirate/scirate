8205d0b1949a2491eee5f79388e1b53e4853eff1
Jaiden Mispy <mispy@draftable.com>
2014-01-07 12:32:09 +1100

    Order feed queries by pubdate last

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -7,7 +7,11 @@ class FeedsController < ApplicationController
   end
 
   def _range_query(feed_ids, backdate, date)
-    paper_ids = Paper.joins(:cross_lists).where("cross_lists.feed_id IN (?) AND cross_lists.cross_list_date >= ? AND cross_lists.cross_list_date <= ?", feed_ids, backdate, date).order("scites_count DESC, comments_count DESC").limit(30).pluck(:id)
+    paper_ids = Paper.joins(:cross_lists)
+                     .where("cross_lists.feed_id IN (?) AND cross_lists.cross_list_date >= ? AND cross_lists.cross_list_date <= ?", feed_ids, backdate, date)
+                     .order("scites_count DESC, comments_count DESC, pubdate DESC")
+                     .limit(30)
+                     .pluck(:id)
     Paper.includes(:feed, :authors, :cross_lists => :feed).where(id: paper_ids).index_by(&:id).slice(*paper_ids).values
   end
 
