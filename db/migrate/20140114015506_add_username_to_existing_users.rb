f0f17fb141c58b556a1402197d242306d6ec06e7
Jaiden Mispy <mispy@draftable.com>
2014-01-14 15:08:43 +1100

    That migration didn't save anything

--- db/migrate/20140114015506_add_username_to_existing_users.rb
+++ db/migrate/20140114015506_add_username_to_existing_users.rb
@@ -2,6 +2,7 @@ class AddUsernameToExistingUsers < ActiveRecord::Migration
   def up
     User.all.each do |user|
       user.username = user.name.downcase.gsub(' ', '.')
+      user.save
     end
   end
 
