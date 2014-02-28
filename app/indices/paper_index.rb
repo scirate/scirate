ce1402e64432a69582b2c4700ab6aef1741e3763
Jaiden Mispy <mispy@draftable.com>
2014-02-28 21:05:09 +1100

    crc32 feed_uids in paper index
    400x speed increase for some queries, by using
    real attributes instead of trying to filter by
    fuzzy text match where it doesn't belong

--- app/indices/paper_index.rb
+++ app/indices/paper_index.rb
@@ -6,5 +6,8 @@ ThinkingSphinx::Index.define :paper, with: :active_record, delta: true do
   indexes authors.searchterm, as: :authors_searchterm
   indexes categories.feed_uid, as: :feed_uids
 
+  # Sphinx has no real conception of a "string attribute", so we must
+  # engage in this madness in order to get efficient category set filtering
+  has "array_to_string(array_agg(crc32(categories.feed_uid)), ',')", as: :feed_uids_filter, type: :integer, multi: :true
   has :scites_count, :comments_count, :submit_date, :update_date, :pubdate
 end
