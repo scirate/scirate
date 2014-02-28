7e51a4c70097876b3dd850c23a2688d569ae9833
Jaiden Mispy <mispy@draftable.com>
2014-02-28 14:10:27 +1100

    Use uglifier for compression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,8 @@ SciRate3::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  config.assets.js_compressor = :uglifier
+
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
 
