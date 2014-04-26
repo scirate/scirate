a52295d33b5e90b0251ffcda3989ace38f21c13f
Trung Lê <trung.le@ruby-journal.com>
2014-04-26 22:32:46 +1000

    Add spec for Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -24,9 +24,7 @@ class Comment < ActiveRecord::Base
   belongs_to :parent, class_name: "Comment" # Immediate reply ancestor
   belongs_to :ancestor, class_name: "Comment" # Highest-level reply ancestor
 
-  validates :user,    presence: true
-  validates :paper,   presence: true
-  validates :content, presence: true
+  validates :user, :paper, :content, presence: true
 
   has_many :reports, class_name: "CommentReport"
   has_many :children, foreign_key: 'parent_id', class_name: 'Comment'
