a32edd44cf46cacad873eb7a8fbfb5efcb495c54
Jaiden Mispy <mispy@draftable.com>
2014-05-10 03:53:55 -0700

    Update search spec

--- spec/search/search_spec.rb
+++ spec/search/search_spec.rb
@@ -1,64 +1,83 @@
 require 'spec_helper'
 
-describe "search migrations" do
+describe "Search#migrate" do
   before :each do
     Search.drop
   end
 
-  it "should create a new index if one doesn't exist" do
-    Search.true_index_name.should be_nil
-    Search.migrate
-    Search.true_index_name.should_not be_nil
+  context "no current search index" do
+    before do
+      expect(Search.true_index_name).to be_nil
+    end
+
+    it "creates a new index" do
+      Search.migrate
+      expect(Search.true_index_name).to_not be_nil
+    end
   end
 
-  it "should migrate if the current mapping is obsolete" do
-    mappings = Search.mappings.dup
-    mappings[:paper][:properties][:foo] = { type: 'string' }
+  context "obsolete search mapping" do
+    before do
+      mappings = Search.mappings.dup
+      mappings[:paper][:properties][:foo] = { type: 'string' }
 
-    Search.es.index(:scirate_test_old).create(settings: Search.settings, mappings: mappings)
-    Search.es.index(:scirate_test_old).alias(:scirate_test).create
-    Search.true_index_name.should == "scirate_test_old"
+      Search.es.index(:scirate_test_old)
+            .create(settings: Search.settings, mappings: mappings)
+      Search.es.index(:scirate_test_old).alias(:scirate_test).create
+      expect(Search.true_index_name).to eq "scirate_test_old"
+    end
 
-    Search.migrate
-    Search.true_index_name.should_not be_nil
-    Search.true_index_name.should_not == "scirate_test_old"
+    it "migrates to a new index with the right mapping" do
+      Search.migrate
+      expect(Search.true_index_name).to_not be_nil
+      expect(Search.true_index_name).to_not eq "scirate_test_old"
+    end
   end
 
-  it "should migrate if the current setting is obsolete" do
-    settings = Search.settings.dup
-    settings[:index][:analysis][:tokenizer][:category_path][:delimiter] = '/'
+  context "obsolete search settings" do
+    before do
+      settings = Search.settings.dup
+      settings[:index][:analysis][:tokenizer][:category_path][:delimiter] = '/'
 
-    Search.es.index(:scirate_test_old).create(settings: settings, mappings: Search.mappings)
-    Search.es.index(:scirate_test_old).alias(:scirate_test).create
-    Search.true_index_name.should == "scirate_test_old"
+      Search.es.index(:scirate_test_old)
+            .create(settings: settings, mappings: Search.mappings)
+      Search.es.index(:scirate_test_old).alias(:scirate_test).create
+      expect(Search.true_index_name).to eq "scirate_test_old"
+    end
 
-    Search.migrate
-    Search.true_index_name.should_not be_nil
-    Search.true_index_name.should_not == "scirate_test_old"
+    it "migrates to a new index with the right settings" do
+      Search.migrate
+      expect(Search.true_index_name).to_not be_nil
+      expect(Search.true_index_name).to_not eq "scirate_test_old"
+    end
   end
 
-  it "should not migrate if the mapping/setting is current" do
-    Search.drop("scirate_test_v1")
-    Search.drop("scirate_test_v2")
+  context "search mappings and settings are current" do
+    before do
+      Search.drop("scirate_test_v1")
+      Search.drop("scirate_test_v2")
+      Search.migrate('v1')
+    end
 
-    Search.migrate('v1')
-    index_name = Search.true_index_name
-    Search.migrate('v2')
-    Search.true_index_name.should == index_name
+    it "does not migrate to a new index" do
+      index_name = Search.true_index_name
+      Search.migrate('v2')
+      expect(Search.true_index_name).to eq index_name
+    end
   end
 end
 
 describe "search fields" do
-  it "should allow searching for a paper by uid" do
+  it "allows searching for a paper by uid" do
     paper1 = FactoryGirl.create(:paper)
     paper2 = FactoryGirl.create(:paper)
 
     Search.refresh
     uids = Search::Paper.query_uids(paper1.uid)
-    uids.should == [paper1.uid]
+    expect(uids).to eq [paper1.uid]
   end
 
-  it "should allow searching for papers in a particular category" do
+  it "allows searching for papers in a particular category" do
     feed = FactoryGirl.create(:feed)
     paper1 = FactoryGirl.create(:paper)
     paper2 = FactoryGirl.create(:paper)
@@ -68,12 +87,12 @@ describe "search fields" do
 
     Search.refresh
     uids = Search::Paper.query_uids("in:#{feed.uid}")
-    uids.should == [paper1.uid]
+    expect(uids).to eq [paper1.uid]
     uids = Search::Paper.query_uids("in:#{feed.uid.split('.')[0]}")
-    uids.should include(paper1.uid)
+    expect(uids).to include paper1.uid
   end
 
-  it "should allow searching for papers scited by a user" do
+  it "allows searching for papers scited by a user" do
     user = FactoryGirl.create(:user)
     user2 = FactoryGirl.create(:user)
     paper1 = FactoryGirl.create(:paper)
@@ -85,15 +104,15 @@ describe "search fields" do
 
     Search.refresh
     uids = Search::Paper.query_uids("scited_by:#{user.username}")
-    uids.should == [paper1.uid]
+    expect(uids).to eq [paper1.uid]
 
     uids = Search::Paper.query_uids("scited_by:#{user2.username}")
-    uids.should == []
+    expect(uids).to eq []
 
     # Make sure this works with duplicate fullnames
     user2.fullname = user.fullname
     user2.save
     uids = Search::Paper.query_uids("scited_by:(#{user.fullname})")
-    uids.should == [paper1.uid]
+    expect(uids).to eq [paper1.uid]
   end
 end
