2d707b13cb2b258646532b107eed842c68feb8c5
Jaiden Mispy <mispy@draftable.com>
2014-06-11 18:57:00 +1000

    Some caching on profile too

--- app/models/authorship.rb
+++ app/models/authorship.rb
@@ -1,6 +1,6 @@
 class Authorship < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :paper, foreign_key: :paper_uid, primary_key: :uid
+  belongs_to :user, touch: true
+  belongs_to :paper, foreign_key: :paper_uid, primary_key: :uid, touch: true
 
   validates :user, presence: true
   validates :paper, presence: true
