e2081b56f2b7df35b3133b319a3b1ee3e2580c97
Jaiden Mispy <mispy@draftable.com>
2014-08-25 16:45:15 +1000

    Slowly media-querifying profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,13 @@ class ApplicationController < ActionController::Base
     return true
   end
 
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+
+  helper_method :mobile_device?
 end
