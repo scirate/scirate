ec1fcaf6c9cbfdcf79a69ec195e63083af2ce415
Jaiden Mispy <jaiden@mispy.me>
2014-10-08 15:55:56 -0700

    Fix tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,4 @@
 module ApplicationHelper
-
-  # Returns the full title on a per-page basis.
-  def full_title(page_title)
-    base_title = "SciRate"
-    if page_title.empty?
-      base_title
-    else
-      sanitize("#{page_title} | #{base_title}")
-    end
-  end
-
   def describe_range(date, range)
     desc = date.to_formatted_s(:rfc822)
     if range != 0
