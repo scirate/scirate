b561bf8c2b4329822edbfd649183ff64e67e43a5
Jaiden Mispy <jaiden@mispy.me>
2014-12-28 01:32:48 +1100

    Test main feed titles more precisely

--- spec/requests/feed_pages_spec.rb
+++ spec/requests/feed_pages_spec.rb
@@ -6,10 +6,10 @@ describe "Feed pages" do
 
   let!(:feed1) { FactoryGirl.create(:feed, last_paper_date: time) }
   let!(:feed2) { FactoryGirl.create(:feed, last_paper_date: time) }
-  let!(:new_paper1) { FactoryGirl.create(:paper, feeds: [feed1], pubdate: time) }
-  let!(:new_paper2) { FactoryGirl.create(:paper, feeds: [feed2], pubdate: time) }
-  let!(:old_paper1) { FactoryGirl.create(:paper, feeds: [feed1], pubdate: time-1.day) }
-  let!(:old_paper2) { FactoryGirl.create(:paper, feeds: [feed2], pubdate: time-1.day) }
+  let!(:new_paper1) { FactoryGirl.create(:paper, title: "New Paper 1", feeds: [feed1], pubdate: time) }
+  let!(:new_paper2) { FactoryGirl.create(:paper, title: "New Paper 2", feeds: [feed2], pubdate: time) }
+  let!(:old_paper1) { FactoryGirl.create(:paper, title: "Old Paper 1", feeds: [feed1], pubdate: time-1.day) }
+  let!(:old_paper2) { FactoryGirl.create(:paper, title: "Old Paper 2", feeds: [feed2], pubdate: time-1.day) }
 
   before do
     Search::Paper.index(new_paper1, new_paper2, old_paper1, old_paper2)
@@ -27,8 +27,8 @@ describe "Feed pages" do
     end
 
     it "shows today's papers" do
-      expect(page).to have_selector("li.paper:nth-child(1)", text: new_paper1.title)
-      expect(page).to have_selector("li.paper:nth-child(2)", text: new_paper2.title)
+      expect(page).to have_selector("li.paper:nth-child(1) .title", text: new_paper1.title)
+      expect(page).to have_selector("li.paper:nth-child(2) .title", text: new_paper2.title)
       expect(page).to_not have_content old_paper1.title
       expect(page).to_not have_content old_paper2.title
     end
