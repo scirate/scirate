6753c11984b74ec59b5dd44af99f6fea5f5e98af
Jaiden Mispy <jaiden@mispy.me>
2015-05-16 17:05:01 -0700

    Set config.assets.digest = true in production
    Seems this setting was lost in the 4.2 update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,10 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
 
+  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
+  # yet still be able to expire them through the digest params.
+  config.assets.digest = true
+
   # Do not fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
