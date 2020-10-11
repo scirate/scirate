d17cb20e223b2e541acfe0295f13991be6ac5e05
Jackson Bates <jacksonbates@hotmail.com>
2020-10-12 08:36:06 +1100

    implements recaptcha v2 (#391)
    * implements recaptcha v3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
+require 'net/https'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  RECAPTCHA_MINIMUM_SCORE = 0.5
 
   include SessionsHelper
 
@@ -16,4 +19,15 @@ class ApplicationController < ActionController::Base
     return true
   end
 
+  def verify_recaptcha?(token, recaptcha_action)
+    
+    uri = URI('https://www.google.com/recaptcha/api/siteverify')
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    req = Net::HTTP::Post.new(uri.path, {'Content-Type' =>'application/json'})
+    req.set_form_data("secret" => Settings::RECAPTCHA_SECRET_KEY, "response" => token)
+    res = http.request(req)
+    json = JSON.parse(res.body)
+    json['success'] && json['score'] > RECAPTCHA_MINIMUM_SCORE && json['action'] == recaptcha_action
+  end
 end
