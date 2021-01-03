d5955d7f235bd7f62e7ecbcb925736116c63f755
silky <noonsilk+-noonsilk@gmail.com>
2021-01-03 11:39:46 +0000

    css

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   config.assets.debug = false
   config.assets.check_precompiled_asset = false
 
-  # config.assets.compile = true
+  config.assets.compile = false
   
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
