0bd7476728980785d2546bf61208f5ad0423480a
silky <noonsilk+-noonsilk@gmail.com>
2021-07-27 17:39:19 +0100

    emails

--- app/mailers/job_mailer.rb
+++ app/mailers/job_mailer.rb
@@ -8,6 +8,6 @@ class JobMailer < ActionMailer::Base
   def job_notification(contact_email, contact_name, job_token)
     @contact_name = contact_name
     @url = Settings::HOST + "/jobs/submit?i=" + job_token
-    mail to: contact_email, subject: "Job submitted to SciRate!"
+    mail to: contact_email, bcc: Settings::SCIRATE_EMAIL_ADMIN, subject: "Job submitted to SciRate!"
   end
 end
