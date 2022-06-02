4a443d20e33c056a7365c5511f9d73bdb5f7d448
Kunal Marwaha <marwahaha@berkeley.edu>
2022-06-02 03:43:25 -0500

    Revert "Add migration file"
    This reverts commit 0a91c5f4521f1c8fb9cc3f14729713bb90b38833.

--- db/migrate/20220531170900_switch_to_https.rb
+++
@@ -1,8 +0,0 @@
-class SwitchToHttps < ActiveRecord::Migration[6.1]
-  def change
-    Paper.find_each do |p|
-      p.pdf_url = p.pdf_url.gsub(/^http:\/\//,'https://')
-      p.save!
-    end
-  end
-end
