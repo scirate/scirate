265890bb102d6baa84bbfa12e13824cbc28c885b
Kunal Marwaha <marwahaha@berkeley.edu>
2023-10-27 05:44:06 -0400

    Set proper cookie settings

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.action_dispatch.cookies_serializer = :marshal
+Rails.application.config.action_dispatch.cookies_same_site_protection = :lax
